2df56835a7c2aa49fa307ec7a9b1cb2083c50b2f
barbara <barbara31fab@gmail.com>
2020-07-06 19:35:41 -0300

    adicionando perguntas e botões

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(new PerguntaApp());
+
+class PerguntaApp extends StatelessWidget {
+  final List<String> perguntas = [
+    'Qual é asua cor favorita?',
+    'Qual é o seu animal favorito?'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Perguntas'),
+          ),
+          body: Column(
+            children: [
+              Text(perguntas.elementAt(0)),
+              RaisedButton(
+                child: Text('Resposta 1'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Resposta 2'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Resposta 3'),
+                onPressed: null,
+              ),
+            ],
+          )),
+    );
+  }
+}
