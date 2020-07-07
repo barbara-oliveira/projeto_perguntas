f9ec0443745b5046989df7bdb20794831d8eb56e
barbara <barbara31fab@gmail.com>
2020-07-07 18:54:20 -0300

    exemplo de funcoes

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,23 @@ import 'package:flutter/material.dart';
 main() => runApp(new PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
-  final List<String> perguntas = [
-    'Qual é asua cor favorita?',
-    'Qual é o seu animal favorito?'
-  ];
+  void responder() {
+    print('Pergunta respondida');
+  }
+
+  void Function() funcaoQueRetornaUmaOutraFuncao() {
+    return () {
+      print('Pergunta respondida #2');
+    };
+  }
 
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
@@ -20,15 +30,15 @@ class PerguntaApp extends StatelessWidget {
               Text(perguntas.elementAt(0)),
               RaisedButton(
                 child: Text('Resposta 1'),
-                onPressed: null,
+                onPressed: responder,
               ),
               RaisedButton(
                 child: Text('Resposta 2'),
-                onPressed: null,
+                onPressed: responder,
               ),
               RaisedButton(
                 child: Text('Resposta 3'),
-                onPressed: null,
+                onPressed: funcaoQueRetornaUmaOutraFuncao(),
               ),
             ],
           )),
